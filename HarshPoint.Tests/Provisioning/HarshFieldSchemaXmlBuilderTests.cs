78a9e84e4b63024fcb68d6e90942633f689ef53b
Julie Koubová <juliekoubova@icloud.com>
2014-11-30 10:25:04 +0100

    Update_with_an_existing_field_calls_only_update_transforms

--- HarshPoint.Tests/Provisioning/HarshFieldSchemaXmlBuilderTests.cs
+++ HarshPoint.Tests/Provisioning/HarshFieldSchemaXmlBuilderTests.cs
@@ -1,4 +1,6 @@
 ﻿using HarshPoint.Provisioning;
+using Microsoft.SharePoint.Client;
+using Moq;
 using System;
 using System.Xml.Linq;
 using Xunit;
@@ -12,7 +14,22 @@ namespace HarshPoint.Tests.Provisioning
         [Fact]
         public void Update_with_an_existing_field_calls_only_update_transforms()
         {
-            throw new NotImplementedException();
+            var addOnlyTransformer = GetNopTransformer();
+            var addOrUpdateTransformer = GetNopTransformer();
+
+            addOnlyTransformer.Object.OnFieldAddOnly = true;
+            addOrUpdateTransformer.Object.OnFieldAddOnly = false;
+
+            var builder = new HarshFieldSchemaXmlBuilder()
+            {
+                Transformers = { addOnlyTransformer.Object, addOrUpdateTransformer.Object }
+            };
+
+            var titleField = GetTitleField();
+            builder.Update(titleField, null);
+
+            addOnlyTransformer.Verify(t => t.Transform(It.IsAny<XElement>()), Times.Never());
+            addOrUpdateTransformer.Verify(t => t.Transform(It.IsAny<XElement>()), Times.Once());
         }
 
         [Fact]
@@ -26,9 +43,7 @@ namespace HarshPoint.Tests.Provisioning
         [Fact]
         public void GetExistingSchemaXml_returns_schema_for_Title_Field()
         {
-            var field = ClientOM.Web.AvailableFields.GetByInternalNameOrTitle("Title");
-            ClientOM.Context.Load(field, f => f.SchemaXmlWithResourceTokens);
-            ClientOM.Context.ExecuteQuery();
+            var field = GetTitleField();
             var expected = XElement.Parse(field.SchemaXmlWithResourceTokens);
             var actual = new HarshFieldSchemaXmlBuilder().GetExistingSchemaXml(field);
 
@@ -38,6 +53,25 @@ namespace HarshPoint.Tests.Provisioning
         public void SetFixture(SharePointClientFixture data)
         {
             ClientOM = data;
+        }
+
+        private Field GetTitleField()
+        {
+            var field = ClientOM.Web.AvailableFields.GetByInternalNameOrTitle("Title");
+            ClientOM.Context.Load(field, f => f.SchemaXmlWithResourceTokens);
+            ClientOM.Context.ExecuteQuery();
+            return field;
+        }
+
+        private Mock<HarshFieldSchemaXmlTransformer> GetNopTransformer()
+        {
+            var mock = new Mock<HarshFieldSchemaXmlTransformer>();
+            
+            mock
+                .Setup(x => x.Transform(It.IsAny<XElement>()))
+                .Returns<XElement>(xe => xe);
+
+            return mock;
         }
     }
 }
