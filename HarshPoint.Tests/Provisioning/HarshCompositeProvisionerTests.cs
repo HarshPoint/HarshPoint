0a98a81d3b109edd110181779619355f721b3eb4
Julie Koubová <juliekoubova@icloud.com>
2014-11-30 16:39:46 +0100

    composite provisioner

---
+++ HarshPoint.Tests/Provisioning/HarshCompositeProvisionerTests.cs
@@ -0,0 +1,53 @@
+﻿using HarshPoint.Provisioning;
+using Moq;
+using Moq.Protected;
+using System;
+using Xunit;
+
+namespace HarshPoint.Tests.Provisioning
+{
+    public class HarshCompositeProvisionerTests
+    {
+        [Fact]
+        public void Calls_children_provision_in_correct_order()
+        {
+            var seq = String.Empty;
+
+            var p1 = new Mock<HarshProvisioner>();
+            var p2 = new Mock<HarshProvisioner>();
+
+            p1.Protected().Setup("OnProvisioning").Callback(() => seq += "1");
+            p2.Protected().Setup("OnProvisioning").Callback(() => seq += "2");
+
+            var composite = new HarshCompositeProvisioner()
+             {
+                 Provisioners = { p1.Object, p2.Object }
+             };
+
+            composite.Provision();
+
+            Assert.Equal("12", seq);
+        }
+
+        [Fact]
+        public void Calls_children_unprovision_in_correct_order()
+        {
+            var seq = String.Empty;
+
+            var p1 = new Mock<HarshProvisioner>();
+            var p2 = new Mock<HarshProvisioner>();
+
+            p1.Protected().Setup("OnUnprovisioning").Callback(() => seq += "1");
+            p2.Protected().Setup("OnUnprovisioning").Callback(() => seq += "2");
+
+            var composite = new HarshCompositeProvisioner()
+             {
+                 Provisioners = { p1.Object, p2.Object }
+             };
+
+            composite.Unprovision();
+
+            Assert.Equal("21", seq);
+        }
+    }
+}
---
+++ HarshPoint/Provisioning/HarshCompositeProvisioner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace HarshPoint.Provisioning
+{
+    public sealed class HarshCompositeProvisioner : HarshProvisioner
+    {
+        public HarshCompositeProvisioner()
+        {
+            Provisioners = new Collection<HarshProvisioner>();
+        }
+
+        protected override void OnProvisioning()
+        {
+            base.OnProvisioning();
+
+            foreach (var provisioner in Provisioners)
+            {
+                provisioner.Context = Context;
+                provisioner.Provision();
+            }
+        }
+
+        protected override void OnUnprovisioning()
+        {
+            foreach (var provisioner in Provisioners.Reverse())
+            {
+                provisioner.Context = Context;
+                provisioner.Unprovision();
+            }
+
+            base.OnUnprovisioning();
+        }
+
+        public Collection<HarshProvisioner> Provisioners
+        {
+            get;
+            private set;
+        }
+    }
+}
