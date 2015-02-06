022ced13b5b54a742944fae359b20c0734fa012a
Julie Koubová <juliekoubova@icloud.com>
2015-02-06 21:49:29 +0100

    HarshTrace

---
+++ HarshPoint/HarshTrace.cs
@@ -0,0 +1,24 @@
+﻿#define DEBUG
+
+using System;
+using System.Diagnostics;
+
+namespace HarshPoint
+{
+    /// <summary>
+    /// Until a better portable tracing solution comes by, use 
+    /// <see cref="System.Diagnostics.Debug"/> even in Release build.
+    /// </summary>
+    internal static class HarshTrace
+    {
+        public static void WriteLine(String message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        public static void WriteLine(String format, params Object[] args)
+        {
+            Debug.WriteLine(format, args);
+        }
+    }
+}
