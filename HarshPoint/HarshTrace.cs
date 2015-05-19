53328b845905cf6f5a8fd0b4e6ef30dc8c62c3e4
Julie Koubová <juliekoubova@icloud.com>
2015-05-19 16:17:18 +0200

    event on harshtrace

--- HarshPoint/HarshTrace.cs
+++ HarshPoint/HarshTrace.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace HarshPoint
 {
@@ -9,16 +10,47 @@ namespace HarshPoint
     /// Until a better portable tracing solution comes by, use 
     /// <see cref="System.Diagnostics.Debug"/> even in Release build.
     /// </summary>
-    internal static class HarshTrace
+    public static class HarshTrace
     {
+        private static readonly Object _syncRoot = new Object();
+
+        private static EventHandler<HarshTraceEventArgs> _onLineWritten =
+            (sender, args) => Debug.WriteLine(args.Message);
+
+        public static event EventHandler<HarshTraceEventArgs> LineWritten
+        {
+            add { lock (_syncRoot) _onLineWritten += value; }
+            remove { lock (_syncRoot) _onLineWritten -= value; }
+        }
+
         public static void WriteLine(String message)
         {
-            Debug.WriteLine(message);
+            _onLineWritten(null, new HarshTraceEventArgs(message));
         }
 
         public static void WriteLine(String format, params Object[] args)
         {
-            Debug.WriteLine(format, args);
+            WriteLine(
+                String.Format(CultureInfo.InvariantCulture, format, args)
+            );
+        }
+    }
+
+    public sealed class HarshTraceEventArgs : EventArgs
+    {
+        public HarshTraceEventArgs(String message)
+        {
+            if (message == null)
+            {
+                throw Error.ArgumentNull(nameof(message));
+            }
+
+            Message = message;
+        }
+        public String Message
+        {
+            get;
+            private set;
         }
     }
 }
