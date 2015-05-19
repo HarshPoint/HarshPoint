96614f97e82b92672f5706d7611977385bdcb928
Julie Koubová <juliekoubova@icloud.com>
2015-05-20 00:42:19 +0200

    HarshSiteGroup

---
+++ HarshPoint/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HarshPoint
+{
+    public static class IEnumerableExtensions
+    {
+        public static TElement FirstOrDefaultByProperty<TElement, TProperty>(
+            this IEnumerable<TElement> sequence, 
+            Func<TElement, TProperty> projection,
+            TProperty value,
+            IEqualityComparer<TProperty> equalityComparer
+        )
+        {
+            if (sequence == null)
+            {
+                throw Error.ArgumentNull(nameof(sequence));
+            }
+
+            if (projection == null)
+            {
+                throw Error.ArgumentNull(nameof(projection));
+            }
+
+            if (equalityComparer == null)
+            {
+                equalityComparer = EqualityComparer<TProperty>.Default;
+            }
+
+            return sequence.FirstOrDefault(element => 
+                equalityComparer.Equals(
+                    projection(element),
+                    value
+                )
+            );
+        }
+    }
+}
---
+++ HarshPoint/Provisioning/HarshSiteGroup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarshPoint.Provisioning
+{
+    public sealed class HarshSiteGroup : HarshProvisioner
+    {
+        public String Description
+        {
+            get;
+            set;
+        }
+
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            var groups = ClientContext.LoadQuery(Web.SiteGroups);
+            await ClientContext.ExecuteQueryAsync();
+
+            Group = groups.FirstOrDefaultByProperty(
+                x => x.Title, 
+                Name, 
+                StringComparer.OrdinalIgnoreCase
+            );
+
+            await base.InitializeAsync();
+        }
+
+        protected override async Task OnProvisioningAsync()
+        {
+            await base.OnProvisioningAsync();
+
+            if (Group.IsNull())
+            {
+                Group = Web.SiteGroups.Add(new GroupCreationInformation()
+                {
+                    Title = Name,
+                    Description = Description,
+                });
+
+                await ClientContext.ExecuteQueryAsync();
+            }
+        }
+
+        private Group Group
+        {
+            get;
+            set;
+        }
+    }
+}
