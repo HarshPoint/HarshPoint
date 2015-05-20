49d932587b67767f38d778d2b7babc4c9d37091b
Julie Koubová <juliekoubova@icloud.com>
2015-05-21 01:07:02 +0200

    GetCSharpSimpleName

---
+++ HarshPoint.Tests/Extensions/TypeInfoExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace HarshPoint.Tests.Extensions
+{
+    public class TypeInfoExtensionsTests
+    {
+        [Theory]
+        [InlineData(typeof(String), "String")]
+        [InlineData(typeof(IEnumerable<>), "IEnumerable<T>")]
+        [InlineData(typeof(IEnumerable<String>), "IEnumerable<String>")]
+        public void GetCSharpSimpleName_returns_expected_values(Type type, String expected)
+        {
+            Assert.Equal(expected, type.GetTypeInfo().GetCSharpSimpleName());
+        }
+    }
+}
---
+++ HarshPoint/Extensions/TypeInfoExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HarshPoint
+{
+    public static class TypeInfoExtensions
+    {
+        public static String GetCSharpSimpleName(this TypeInfo typeInfo)
+        {
+            if (typeInfo == null)
+            {
+                throw Error.ArgumentNull(nameof(typeInfo));
+            }
+
+            var nameWithoutArgCount = Regex.Replace(typeInfo.Name, "`\\d+$", String.Empty);
+            var result = new StringBuilder(nameWithoutArgCount);
+
+            if (typeInfo.IsGenericTypeDefinition)
+            {
+                AppendGenericArgumentsOrParameters(
+                    result,
+                    typeInfo.GenericTypeParameters
+                );
+
+            }
+            else if (typeInfo.IsGenericType)
+            {
+                AppendGenericArgumentsOrParameters(
+                    result,
+                    typeInfo.GenericTypeArguments
+                );
+            }
+
+            return result.ToString();
+        }
+
+        private static void AppendGenericArgumentsOrParameters(StringBuilder result, IEnumerable<Type> argsOrParams)
+        {
+            if (!argsOrParams.Any())
+            {
+                return;
+            }
+
+            const String Delimiter = ", ";
+
+            result.Append('<');
+
+            foreach (var item in argsOrParams)
+            {
+                result.Append(GetCSharpSimpleName(item.GetTypeInfo()));
+                result.Append(Delimiter);
+            }
+
+            result.Length -= Delimiter.Length;
+            result.Append('>');
+        }
+    }
+}
