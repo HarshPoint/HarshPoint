3b69d28bd9ed0b119910bf2632cdd82f3b22bc0e
Julie Koubová <juliekoubova@icloud.com>
2015-06-04 21:34:28 +0200

    ShellPloy

---
+++ HarshPoint.Shellploy/HarshPoint.Shellploy.psm1
@@ -0,0 +1,116 @@
+﻿
+function New-HarshProvisioner {
+
+    param (
+        [String]
+        $Name,
+
+        [ScriptBlock]
+        $Children,
+
+        [Hashtable]
+        $Property
+    )
+
+    $Result = New-Object "HarshPoint.Provisioning.$Name" -Property $Property
+
+    if ($Children) {
+        & $Children |% { $Result.Children.Add($_) }
+    }
+
+    $Result
+}
+
+function ContentType {
+
+    param (
+        [HarshPoint.HarshContentTypeId]
+        $Id,
+
+        [String]
+        $Name,
+
+        [String]
+        $Group,
+         
+        [ScriptBlock]
+        $Children
+    )
+
+    New-HarshProvisioner HarshContentType $Children @{
+        Id    = $Id
+        Name  = $Name
+        Group = $Group
+    }
+}
+
+function Field {
+
+    param (
+        [Guid]
+        $Id,
+
+        [String]
+        $InternalName,
+
+        [String]
+        $DisplayName,
+
+        [String]
+        $Type,
+         
+        [ScriptBlock]
+        $Children
+    )
+
+    New-HarshProvisioner HarshFieldSchemaXmlProvisioner $Children @{
+        DisplayName   = $DisplayName
+        Id            = $Id
+        InternalName  = $InternalName
+        TypeName      = $Type
+    }
+}
+
+function FieldRef {
+
+    param (
+        [String]
+        $InternalName
+    )
+
+    New-HarshProvisioner HarshFieldRef $null @{
+        Fields = [HarshPoint.Provisioning.Resolve]::FieldByInternalName($InternalName)
+    }
+}
+
+
+function Provision {
+
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Position = 0)]
+        [String]
+        $Url,
+
+        [Parameter(Position = 1)]
+        [ScriptBlock]
+        $Children,
+
+        [Parameter()]
+        [String]
+        $UserName,
+        
+        [Parameter()]
+        [String]
+        $Password
+    )
+
+    $Context = [HarshPoint.Provisioning.HarshProvisionerContext]::OpenSharePointOnline(
+        $Url, $UserName, $Password
+    )
+
+
+    $Provisioner = New-HarshProvisioner HarshProvisioner $Children
+    $Provisioner.ProvisionAsync($Context).Wait()
+}
