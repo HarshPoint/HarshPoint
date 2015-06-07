5b95fcb53c884518e73d9d3bb84fb14b3f6da38d
Julie Koubová <juliekoubova@icloud.com>
2015-06-07 20:11:36 +0200

    HarshSingleElementCollection

---
+++ HarshPoint.Tests/SingleElementCollection.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace HarshPoint.Tests
+{
+    public class SingleElementCollection
+    {
+        [Fact]
+        public void Returns_single_value()
+        {
+            var enumerable = HarshSingleElementCollection.Create(42);
+            Assert.Single(enumerable, 42);
+        }
+    }
+}
