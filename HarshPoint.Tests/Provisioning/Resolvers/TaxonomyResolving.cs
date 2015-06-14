983db122a8603439582c5e0077d3955f88d34ebc
Julie Koubová <juliekoubova@icloud.com>
2015-06-15 01:15:53 +0200

    termset resolving

--- HarshPoint.Tests/Provisioning/Resolvers/TaxonomyResolving.cs
+++ HarshPoint.Tests/Provisioning/Resolvers/TaxonomyResolving.cs
@@ -1,5 +1,7 @@
 ﻿using HarshPoint.Provisioning;
 using Microsoft.SharePoint.Client.Taxonomy;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -7,6 +9,9 @@ namespace HarshPoint.Tests.Provisioning.Resolvers
 {
     public class TaxonomyResolving : IClassFixture<SharePointClientFixture>
     {
+        private static readonly Guid GroupId = new Guid("6e3c51c5-9199-4b0e-bb81-00b8ec433740");
+        private static readonly Guid TermSetId = new Guid("a5d59d30-10e8-4221-bf59-75a1d76f0be0");
+
         public TaxonomyResolving(SharePointClientFixture fix)
         {
             Fixture = fix;
@@ -44,6 +49,57 @@ namespace HarshPoint.Tests.Provisioning.Resolvers
             await Fixture.ClientContext.ExecuteQueryAsync();
 
             Assert.Equal(expected.Id, termStore.Id);
+        }
+
+        [Fact]
+        public async Task TermSet_resolved_by_id()
+        {
+            var expected = await EnsureTestTermSet();
+
+            IResolve<TermSet> resolver = Resolve.TermStoreSiteCollectionDefault().TermSetById(TermSetId);
+            var actual = await resolver.ResolveSingleAsync(Fixture.ResolveContext);
+
+            Fixture.ClientContext.Load(actual, ts => ts.Id, ts => ts.Name);
+            await Fixture.ClientContext.ExecuteQueryAsync();
+
+            Assert.Equal(TermSetId, actual.Id);
+            Assert.Equal(TermSetId.ToString("n"), actual.Name);
+        }
+
+        private async Task<TermSet> EnsureTestTermSet()
+        {
+            var store = Fixture.TaxonomySession.GetDefaultSiteCollectionTermStore();
+            var group = store.GetGroup(GroupId);
+
+            try
+            {
+                await Fixture.ClientContext.ExecuteQueryAsync();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                if (group.IsNull())
+                {
+                    group = store.CreateGroup(GroupId.ToString("n"), GroupId);
+                }
+            }
+
+            var termSet = group.TermSets.GetById(TermSetId);
+
+            try
+            {
+                await Fixture.ClientContext.ExecuteQueryAsync();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                if (termSet.IsNull())
+                {
+                    termSet = group.CreateTermSet(TermSetId.ToString("n"), TermSetId, 1033);
+                }
+
+                await Fixture.ClientContext.ExecuteQueryAsync();
+            }
+
+            return termSet;
         }
     }
 }
