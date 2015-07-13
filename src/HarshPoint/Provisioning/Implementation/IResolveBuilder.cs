1a654368bceb2cc5f17bca7b00c4156f5409b5b3
Julie Koubová <juliekoubova@icloud.com>
2015-07-13 23:13:46 +0200

    iresolvebuilder two files

--- src/HarshPoint/Provisioning/Implementation/IResolveBuilder.cs
+++ src/HarshPoint/Provisioning/Implementation/IResolveBuilder.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections;
-
-namespace HarshPoint.Provisioning.Implementation
+﻿namespace HarshPoint.Provisioning.Implementation
 {
     public interface IResolveBuilder
     {
-    }
-
-    public interface IResolveBuilder<TContext> : IResolveBuilder
-        where TContext : HarshProvisionerContextBase
-    {
-        Object Initialize(ResolveContext<TContext> context);
-
-        IEnumerable ToEnumerable(Object state, ResolveContext<TContext> context);
     }
 }
---
+++ src/HarshPoint/Provisioning/Implementation/IResolveBuilder`1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+namespace HarshPoint.Provisioning.Implementation
+{
+    public interface IResolveBuilder<TContext> : IResolveBuilder
+        where TContext : HarshProvisionerContextBase
+    {
+        Object Initialize(ResolveContext<TContext> context);
+
+        IEnumerable ToEnumerable(Object state, ResolveContext<TContext> context);
+    }
+}
