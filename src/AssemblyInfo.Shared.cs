4dbd2ae0f3cc8f2152495804ef8be7fc51515022
Julie Koubová <juliekoubova@icloud.com>
2015-07-21 16:36:25 +0200

    fixed wrong paths

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 using System;
 
 [assembly: AssemblyProduct("HarshPoint")]
-[assembly: AssemblyCopyright("Copyright ©  2014 Julie Koubová")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 Julie Koubová")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
