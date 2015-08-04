bc3408b6a5ce00c71394e2cf7ea8858475c4d1df
Julie Koubová <juliekoubova@icloud.com>
2015-08-04 18:31:00 +0200

    strict ClientObjectIdentifierResolveBuilder

--- src/HarshPoint/Provisioning/Implementation/ClientObjectIdentifierResolveBuilder`2.cs
+++ src/HarshPoint/Provisioning/Implementation/ClientObjectIdentifierResolveBuilder`2.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Collections;
 
 namespace HarshPoint.Provisioning.Implementation
 {
@@ -10,32 +11,13 @@ namespace HarshPoint.Provisioning.Implementation
         where TResult : ClientObject
     {
         protected ClientObjectIdentifierResolveBuilder(
-            IResolveBuilder<TResult> parent, 
-            IEnumerable<TIdentifier> identifiers
-        ) 
-            : base(parent, identifiers)
-        {
-        }
-
-        protected ClientObjectIdentifierResolveBuilder(
             IResolveBuilder<TResult> parent,
             IEnumerable<TIdentifier> identifiers,
             Expression<Func<TResult, TIdentifier>> identifierExpression
         )
-            : base(parent, identifiers)
+            : this(parent, identifiers, null, identifierExpression)
         {
-            IdentifierExpression = identifierExpression;
-            IdentifierSelector = identifierExpression?.Compile();
-        }
 
-        protected ClientObjectIdentifierResolveBuilder(
-            IResolveBuilder<TResult> parent, 
-            IEnumerable<TIdentifier> identifiers, 
-            IEqualityComparer<TIdentifier> identifierComparer
-        ) 
-        : 
-            base(parent, identifiers, identifierComparer)
-        {
         }
 
         protected ClientObjectIdentifierResolveBuilder(
@@ -47,46 +29,52 @@ namespace HarshPoint.Provisioning.Implementation
         :
             base(parent, identifiers, identifierComparer)
         {
-            IdentifierExpression = identifierExpression;
-            IdentifierSelector = identifierExpression?.Compile();
+            if (identifierExpression == null)
+            {
+                throw Logger.Fatal.ArgumentNull(nameof(identifierExpression));
+            }
 
+            IdentifierExpression = identifierExpression.ConvertToObject();
+            IdentifierSelector = identifierExpression.Compile();
         }
 
-        protected Expression<Func<TResult, TIdentifier>> IdentifierExpression
-        {
-            get; private set;
-        }
+        protected sealed override TIdentifier GetIdentifierFromItem(Object item)
+            => base.GetIdentifierFromItem(item);
 
-        protected Func<TResult, TIdentifier> IdentifierSelector
-        {
-            get; private set;
-        }
+        protected sealed override TIdentifier GetIdentifierFromNested(NestedResolveResult nested)
+            => base.GetIdentifierFromNested(nested);
 
-        protected override void InitializeContextBeforeParent(ClientObjectResolveContext context)
+        protected sealed override TIdentifier GetIdentifier(TResult result)
+            => IdentifierSelector(result);
+
+        protected sealed override Object Initialize(ClientObjectResolveContext context)
+            => base.Initialize(context);
+
+        protected sealed override void InitializeContextBeforeParent(ClientObjectResolveContext context)
         {
             if (context == null)
             {
                 throw Logger.Fatal.ArgumentNull(nameof(context));
             }
 
-            if (IdentifierExpression != null)
-            {
-                context.Include(
-                    IdentifierExpression.ConvertToObject()
-                );
-            }
+            context.Include(
+                IdentifierExpression
+            );
 
             base.InitializeContextBeforeParent(context);
         }
 
-        protected override TIdentifier GetIdentifier(TResult result)
-        {
-            if (IdentifierSelector != null)
-            {
-                return IdentifierSelector(result);
-            }
+        protected sealed override IEnumerable ToEnumerable(Object state, ClientObjectResolveContext context)
+            => base.ToEnumerable(state, context);
 
-            return base.GetIdentifier(result);
+        private Expression<Func<TResult, Object>> IdentifierExpression
+        {
+            get; set;
+        }
+
+        private Func<TResult, TIdentifier> IdentifierSelector
+        {
+            get; set;
         }
 
         private static readonly HarshLogger Logger = HarshLog.ForContext(typeof(ClientObjectIdentifierResolveBuilder<,>));
