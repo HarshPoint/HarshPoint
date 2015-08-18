1a5f14e05003bfab2b0dcbe5ab627ed6737a7576
Julie Koubová <juliekoubova@icloud.com>
2015-08-18 21:14:23 +0200

    coverity...

--- Invoke-CoverityBuild.ps1
+++ Invoke-CoverityBuild.ps1
@@ -4,11 +4,24 @@ function New-StringContent($str) {
 	New-Object Net.Http.StringContent $str
 }
 
-$Env:Path = "${Env:ProgramFiles(x86)}\MSBuild\14.0\bin\;${Env:Path}"
+function New-MultipartFormDataContent([hashtable]$fields) {
+	
+	$form = New-Object Net.Http.MultipartFormDataContent
+
+	$fields.Keys |% {
+		$form.Add($field[$_], "`"$_`"")
+	}
+
+	$form
+}
+
+$Env:Path    = "${Env:ProgramFiles(x86)}\MSBuild\14.0\bin\;${Env:Path}"
+$CoverityZip = "${Env:APPVEYOR_PROJECT_NAME}.zip"
 
 $MSBuildArgs = @(
-  '/m'
-  "/l:${Env:ProgramFiles}\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	'/m'
+	'/verbosity:quiet'
+	"/l:${Env:ProgramFiles}\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 )
 
 if (${Env:CONFIGURATION}) {
@@ -17,58 +30,66 @@ if (${Env:CONFIGURATION}) {
 
 if (${Env:PLATFORM}) {
 	$MSBuildArgs += @("/p:Platform=${Env:PLATFORM}")
-}  
+}
 
-if (($Env:APPVEYOR_SCHEDULED_BUILD -ne 'True') -and
-	($Env:APPVEYOR_FORCED_BUILD    -ne 'True')) {
+function Invoke-MSBuild {
+	Write-Host "Building project without Coverity Scan..."
 	msbuild $MSBuildArgs
 	exit $LastExitCode
 }
 
-$CoverityZip = "${Env:APPVEYOR_PROJECT_NAME}.zip"
+function Invoke-Coverity {
 
-Write-Host "Building project with Coverity Scan..."
-cov-build --dir cov-int msbuild  $MSBuildArgs
-if (-not $?) { exit $LastExitCode }
+	Write-Host "Building project with Coverity Scan..."
+	cov-build --dir cov-int msbuild  $MSBuildArgs
+	if (-not $?) { exit $LastExitCode }
 
-Write-Host "Compressing Coverity results for upload..."
-7z a $CoverityZip cov-int\*
-if (-not $?) { exit $LastExitCode }
+	Write-Host "Compressing Coverity results for upload..."
+	7z a $CoverityZip cov-int\*
+	if (-not $?) { exit $LastExitCode }
 
-Write-Host "Uploading Coverity results..."
+	Write-Host "Uploading Coverity results..."
 
-$fields = @{
-	token       = New-StringContent $Env:CoverityProjectToken
-	email       = New-StringContent $Env:CoverityNotificationEmail
-	version     = New-StringContent $Env:APPVEYOR_BUILD_VERSION
-	description = New-StringContent 'AppVeyor scheduled build'
+	$form = New-MultipartFormDataContent @{
+		token       = New-StringContent $Env:CoverityProjectToken
+		email       = New-StringContent $Env:CoverityNotificationEmail
+		version     = New-StringContent $Env:APPVEYOR_BUILD_VERSION
+		description = New-StringContent 'AppVeyor scheduled build'
+	}
+
+	$path   = (Resolve-Path $CoverityZip).Path
+	$stream = [IO.File]::OpenRead($path)
+
+	$file = New-StreamContent $CoverityZip
+	$form.Add($file, '"file"', (Split-Path -Leaf $CoverityZip))
+
+	$client = New-Object Net.Http.HttpClient -Property @{
+		Timeout = [TimeSpan]::FromMinutes(20)
+	}
+
+	$url = "https://scan.coverity.com/builds?project=${Env:APPVEYOR_REPO_NAME}"
+	$task = $client.PostAsync($url, $form)
+
+	try {
+		$task.Wait()
+	} 
+	catch [AggregateException] {
+		throw $_.Exception.InnerException
+	}
+
+	$task.Result
+	$stream.Close()
 }
 
-$form = New-Object Net.Http.MultipartFormDataContent
+Write-Host "APPVEYOR_SCHEDULED_BUILD=${Env:APPVEYOR_SCHEDULED_BUILD}"
+Write-Host "APPVEYOR_FORCED_BUILD=${Env:APPVEYOR_FORCED_BUILD}"
 
-$fields.Keys |% {
-	$form.Add($field[$_], "`"$_`"")
+if ($Env:APPVEYOR_SCHEDULED_BUILD -eq 'True') {
+	Invoke-Coverity
 }
-
-$path   = (Resolve-Path $CoverityZip).Path
-$stream = [IO.File]::OpenRead($path)
-
-$file = New-StreamContent $CoverityZip
-$form.Add($file, '"file"', (Split-Path -Leaf $CoverityZip))
-
-$client = New-Object Net.Http.HttpClient -Property @{
-	Timeout = [TimeSpan]::FromMinutes(20)
+elseif ($Env:APPVEYOR_FORCED_BUILD -eq 'True') {
+	Invoke-Coverity
 }
-
-$url = "https://scan.coverity.com/builds?project=${Env:APPVEYOR_REPO_NAME}"
-$task = $client.PostAsync($url, $form)
-
-try {
-	$task.Wait()
-} 
-catch [AggregateException] {
-	throw $_.Exception.InnerException
+else {
+	Invoke-MSBuild
 }
-
-$task.Result
-$stream.Close()
