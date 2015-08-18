2f6d46b2b4c7ad5053024264420a139e594bda89
Julie Koubová <juliekoubova@icloud.com>
2015-08-18 18:40:11 +0200

    description

--- src/HarshPoint/Properties/AssemblyInfo.cs
+++ src/HarshPoint/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("HarshPoint")]
+[assembly: AssemblyDescription("A provisioning framework for SharePoint using the client side object model.")]
 [assembly: InternalsVisibleTo("HarshPoint.Server")]
 [assembly: InternalsVisibleTo("HarshPoint.Tests")]
 [assembly: InternalsVisibleTo("HarshPoint.ShellployGenerator")]
