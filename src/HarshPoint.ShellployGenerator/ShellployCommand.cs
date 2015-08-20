931e4c6b7bf53b88d70faa9897a344562aa95140
Julie Koubová <juliekoubova@icloud.com>
2015-08-20 22:56:21 +0200

    usings

--- src/HarshPoint.ShellployGenerator/ShellployCommand.cs
+++ src/HarshPoint.ShellployGenerator/ShellployCommand.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq.Expressions;
-using System.Reflection;
 
 namespace HarshPoint.ShellployGenerator
 {
