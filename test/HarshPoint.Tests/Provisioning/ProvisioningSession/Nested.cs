beac98e08925004a4f8b1e0d35fd6b119436e33d
terka <tereza.tomcova@gmail.com>
2015-08-27 13:54:38 +0200

    Do not skip children when skipping parent, more tests for ProvisioningSession

---
+++ test/HarshPoint.Tests/Provisioning/ProvisioningSession/Nested.cs
@@ -0,0 +1,77 @@
+﻿using HarshPoint.ObjectModel;
+using HarshPoint.Provisioning;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using HarshPoint.Provisioning.Implementation;
+using Moq;
+using Moq.Sequences;
+
+namespace ProvisioningSession
+{
+    public class Nested : ProvisioningSessionTest
+    {
+        public Nested(ITestOutputHelper output)
+        : base(output)
+        {
+        }
+
+        private HarshProvisioner Provisioner
+            => new HarshProvisioner()
+            {
+                Children =
+                {
+                    new HarshChild1()
+                    {
+                        Children =
+                        {
+                            new HarshSubChild(),
+                        },
+                    },
+                },
+            };
+
+        [Fact]
+        public async Task Provisioning_inspector_notifications_are_in_correct_order()
+        {
+            var mockInspector = new Mock<IProvisioningSessionInspector>(MockBehavior.Strict);
+
+            using (Sequence.Create())
+            {
+                AddSessionStartingSequence(mockInspector);
+
+                AddProvisioningSequence<HarshProvisioner>(mockInspector);
+                AddProvisioningSequence<HarshChild1>(mockInspector);
+                AddProvisioningSequence<HarshSubChild>(mockInspector);
+
+                AddSessionEndedSequence(mockInspector);
+
+                var context = Context.AddSessionInspector(mockInspector.Object);
+                await Provisioner.ProvisionAsync(context);
+            }
+        }
+
+        [Fact]
+        public async Task Unprovisioning_inspector_notifications_are_in_correct_order()
+        {
+            var mockInspector = new Mock<IProvisioningSessionInspector>(MockBehavior.Strict);
+
+            using (Sequence.Create())
+            {
+                AddSessionStartingSequence(mockInspector);
+
+                AddProvisioningSequence<HarshProvisioner>(mockInspector);
+                AddProvisioningSequence<HarshChild1>(mockInspector);
+                AddProvisioningSequence<HarshSubChild>(mockInspector);
+
+                AddSessionEndedSequence(mockInspector);
+
+                var context = Context.AddSessionInspector(mockInspector.Object);
+                await Provisioner.UnprovisionAsync(context);
+            }
+        }
+    }
+}
---
+++ test/HarshPoint.Tests/Provisioning/ProvisioningSession/ProvisioningSessionTest.cs
@@ -0,0 +1,76 @@
+﻿using HarshPoint.Provisioning;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using HarshPoint.Provisioning.Implementation;
+using Moq;
+using Moq.Sequences;
+using HarshPoint.Tests;
+
+namespace ProvisioningSession
+{
+    public abstract class ProvisioningSessionTest : SharePointClientTest
+    {
+        public ProvisioningSessionTest(ITestOutputHelper output)
+        : base(output)
+        {
+        }
+
+        protected class HarshProvisionerSkipped : HarshProvisioner
+        {
+            // Missing HarshPoint.Provisioning.NeverDeletesUserData
+            protected override Task OnUnprovisioningAsync()
+            {
+                return base.OnUnprovisioningAsync();
+            }
+        }
+
+        protected class HarshChild1 : HarshProvisioner
+        {
+        }
+
+        protected class HarshChild2 : HarshProvisioner
+        {
+        }
+
+        protected class HarshSubChild : HarshProvisioner
+        {
+        }
+
+        protected void AddProvisioningSequence<TProvisioner>(Mock<IProvisioningSessionInspector> mockInspector)
+        where TProvisioner : HarshProvisionerBase
+        {
+            mockInspector.Setup(x => x.OnProvisioningStarting(
+                It.IsAny<HarshProvisionerContext>(),
+                It.IsAny<TProvisioner>()
+            )).InSequence();
+            mockInspector.Setup(x => x.OnProvisioningEnded(
+                It.IsAny<HarshProvisionerContext>(),
+                It.IsAny<TProvisioner>()
+            )).InSequence();
+        }
+
+        protected void AddProvisioningSkippedSequence<TProvisioner>(Mock<IProvisioningSessionInspector> mockInspector)
+            where TProvisioner : HarshProvisionerBase
+        {
+            mockInspector.Setup(x => x.OnProvisioningSkipped(
+                It.IsAny<HarshProvisionerContext>(),
+                It.IsAny<TProvisioner>()
+            )).InSequence();
+        }
+
+        protected void AddSessionStartingSequence(Mock<IProvisioningSessionInspector> mockInspector)
+        {
+            mockInspector.Setup(x => x.OnSessionStarting(
+                It.IsAny<HarshProvisionerContext>()
+            )).InSequence();
+        }
+
+        protected void AddSessionEndedSequence(Mock<IProvisioningSessionInspector> mockInspector)
+        {
+            mockInspector.Setup(x => x.OnSessionEnded(
+                It.IsAny<HarshProvisionerContext>()
+            )).InSequence();
+        }
+    }
+}
