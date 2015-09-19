8d18a4f54cedfdd5ead3200919fc92805f47d39f
Tereza Tomcova <tereza.tomcova@gmail.com>
2015-09-19 14:30:57 +0200

    Added HarshModifyFieldUser

---
+++ src/HarshPoint/Provisioning/HarshModifyFieldUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+
+namespace HarshPoint.Provisioning
+{
+    public sealed class HarshModifyFieldUser :
+        HarshModifyField<FieldUser, HarshModifyFieldUser>
+    {
+        public HarshModifyFieldUser()
+        {
+            Map(f => f.SelectionMode);
+            Map(f => f.SelectionGroup);
+        }
+
+        [Parameter]
+        public FieldUserSelectionMode SelectionMode { get; set; }
+
+        [Parameter]
+        public Int32 SelectionGroup { get; set; }
+    }
+}
